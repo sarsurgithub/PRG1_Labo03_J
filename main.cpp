0aba4d58ab6d74ee44f50d61920c5912466c37d0
Sar <sarah.jallon@gmail.com>
2020-10-27 09:29:40 +0100

    entete

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
-#include <iostream>
+/*
+  ---------------------------------------------------------------------------
+  Fichier     : main.cpp
+  Nom du labo : Calendrier
+  Auteur.e.s  : Sarah Jallon, Davide
+  Date        : 27/10/2020
+  But         : le but du programme et non le but du laboratoire !!
 
+  Remarque(s) : à compléter
+
+  Compilateur : Mingw-w64 g++ 8.1.0
+  ---------------------------------------------------------------------------
+*/
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+
+ return EXIT_SUCCESS;
 }
