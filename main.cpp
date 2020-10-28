2ddb55147aacd32e317a2ff3bf593454b92a09c3
Sar <sarah.jallon@gmail.com>
2020-10-28 14:35:26 +0100

    BEAUCOUP DE CHOSES DESO

--- main.cpp
+++ main.cpp
@@ -17,9 +17,15 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <iomanip>
+
 using namespace std;
 
-#define CLEAR_BUFFER cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define IGNORE_BUFFER cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define CLEAR_BUFFER  cin.clear()
+
+enum class Mois {JANVIER, FEVRIER, MARS, AVRIL, MAI, JUIN ,JUILLET, AOUT, SEPTEMBRE, OCTOBRE, NOVEMBRE, DECEMBRE};
+
 
 int main() {
 
@@ -29,25 +35,108 @@ int main() {
  const int  ANNEE_FIN    = 2100;
  const char TIRET        = '-';
 
+ char recommencer;
+
  // ----------------------------------------------------------------------------------------------
  // Introduction du programme
  cout << "Ce programme vous affiche le calendrier de l'annee que vous desirez." << endl;
 
  // ----------------------------------------------------------------------------------------------
- //
+ // Entrée utilisateur et contrôles des valeurs et des types de valeurs.
 
- int annee_choisie;
- cout << "Entrer une valeur [" << ANNEE_DEPART << TIRET << ANNEE_FIN << "] :" << endl;
- cin >> annee_choisie;
- while (annee_choisie > ANNEE_FIN || annee_choisie < ANNEE_DEPART || !cin) {
- 	cin.clear();
- 	CLEAR_BUFFER;
+ int  anneeChoisie;
+ bool bonneAnnee;
+do{
+ do {
+	cout << "Entrer une valeur [" << ANNEE_DEPART << TIRET << ANNEE_FIN << "] :";
+	cin >> anneeChoisie;
 
- 	cout << "/!\\ recommencer" << endl;
- 	cout << "Entrer une valeur [" << ANNEE_DEPART << TIRET << ANNEE_FIN << "] :";
- 	cin >> annee_choisie;
+	bonneAnnee = anneeChoisie > ANNEE_FIN || anneeChoisie < ANNEE_DEPART || cin.fail();
+	if(bonneAnnee){
+ 	 cout << "/!\\ recommencer" << endl;
+		CLEAR_BUFFER;
+		IGNORE_BUFFER;
+	}
+
+ }  while (bonneAnnee);
+
+ // ----------------------------------------------------------------------------------------------
+ //Chercher si notre année est bissextile
+	bool bissextile = false;
+
+	if ((anneeChoisie%4 == 0 && anneeChoisie%100 != 0) || ( anneeChoisie%400 == 0)) {
+		bissextile = true;
+	}
+
+
+ // ----------------------------------------------------------------------------------------------
+ //Affichage les mois dans l'ordre
+
+ cout << anneeChoisie << " " << bissextile;
+	unsigned jours;
+ for (int i=1; i < 13; i++) {
+
+	 switch (Mois(i - 1)) {
+		 case Mois::JANVIER  : cout << "JANVIER" ;
+		 							  jours = 31; break;
+		 case Mois::FEVRIER  : if(bissextile) {
+		 						      jours = 28;
+		 				           } else {
+		 	                     jours = 29;
+		                       };
+		                       cout << "FEVRIER " << anneeChoisie;
+		                       break;
+		 case Mois::MARS     : cout << "MARS"    ;
+		 							  jours = 31; break;
+		 case Mois::AVRIL    : cout << "AVRIL"   ;
+		                       jours << 30; break;
+		 case Mois::MAI      : cout << "MAI"     ;
+		 							  jours = 31; break;
+		 case Mois::JUIN     : cout << "JUIN"     ;
+		                       jours = 30; break;
+		 case Mois::JUILLET  : cout << "JUILLET" ;
+		 							  jours = 31; break;
+		 case Mois::AOUT     : cout << "AOUT"    ;
+		 							  jours = 31; break;
+		 case Mois::SEPTEMBRE: cout << "SEPTEMBRE ";
+		                       jours = 30; break;
+		 case Mois::OCTOBRE  : cout << "OCTOBRE " ;
+		 							  jours = 31; break;
+		 case Mois::NOVEMBRE : cout << "NOVEMBRE ";
+		 	                    jours = 30; break;
+		 case Mois::DECEMBRE : cout << "DECEMBRE ";
+		 						     jours = 31; break;
+	 }
+	 cout << anneeChoisie << endl;
+	const int N_DEPART = 1;
+	const int NB_COLONNES = 7;
+	 unsigned n = N_DEPART, noColonne = 1;
+	 while (n <= jours) {
+		 cout << setw(2) << n; // affiche le nombre @SAR : Insérer une constante 2
+		 if (noColonne == NB_COLONNES) {
+			 cout << endl; // change de ligne
+			 noColonne = 0;
+		 } else {
+			 if (n != jours) {
+				 cout << " ";
+			 }
+		 }
+		 noColonne++;
+		 n++;
+	 }
+	 cout << endl << endl;
+
+
  }
- cout << annee_choisie;
 
- return EXIT_SUCCESS;
+ // ----------------------------------------------------------------------------------------------
+ // fin de programme
+	do {
+		cout << "voulez-vous recommencer [O/N]";
+		cin  >> recommencer;
+	} while (recommencer != 'N' && recommencer != 'O');
+
+	} while ( recommencer == 'O');
+	IGNORE_BUFFER;
+	return EXIT_SUCCESS;
 }
